c395b81cfda504700629fbc463f8bd0fe19c6faf
Saetbyeol Lim <saetbyeol701@gmail.com>
2024-06-05 12:01:31 -0400

    Commentapp & Mixin

---
+++ commentapp/urls.py
@@ -0,0 +1,25 @@
+from django.urls import path
+
+from commentapp.views import CommentCreateView
+
+app_name = 'commentapp'
+
+urlpatterns = [
+    path('create/', CommentCreateView.as_view(), name='create'),
+
+]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
