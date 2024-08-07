e5a9890c89330eca4601efc6c6071c9e5cee9ef6
Saetbyeol Lim <saetbyeol701@gmail.com>
2024-08-07 14:22:49 -0400

    Added 'Like' Message

--- stellagraphy/settings/base.py
+++ stellagraphy/settings/base.py
@@ -14,6 +14,7 @@ from pathlib import Path
 
 import os, environ
 
+from django.contrib.messages import constants as messages
 from django.urls import reverse_lazy
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -48,6 +49,10 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',
+}
 
 ROOT_URLCONF = 'stellagraphy.urls'
 
